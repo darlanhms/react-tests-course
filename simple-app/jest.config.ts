10a7bb129e46c043b9b8a6b00e5ce9182d59e0ab
Darlan H <darlanhermessjb@hotmail.com>
2022-09-17 16:28:30 -0300

    config eslint

--- simple-app/jest.config.ts
+++ simple-app/jest.config.ts
@@ -5,17 +5,15 @@ export default {
   clearMocks: true,
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
-  transform: {'^.+\.(ts|tsx)$': 'ts-jest'},
+  transform: { '^.+.(ts|tsx)$': 'ts-jest' },
   roots: ['<rootDir>/src'],
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  testPathIgnorePatterns: ['/node_modules/'],
   testRegex: '.(spec|test).(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
-},
-}
+  },
+};
--- simple-app/src/App.spec.tsx
+++ simple-app/src/App.spec.tsx
@@ -1,12 +1,12 @@
-import {render, screen} from '@testing-library/react'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 describe('Test App', () => {
-    it('should render App with a link', () => {
-        render(<App/>);
+  it('should render App with a link', () => {
+    render(<App />);
 
-        const linkElement = screen.getByTestId('learn-link') as HTMLLinkElement;
-        expect(linkElement).toBeInTheDocument();
-        expect(linkElement.href).toMatch('reactjs.org');
-    })
-})
+    const linkElement = screen.getByTestId('learn-link') as HTMLLinkElement;
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement.href).toMatch('reactjs.org');
+  });
+});
